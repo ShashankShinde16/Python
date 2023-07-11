523e4a4437f960292a4ca59985e65abed6f5ed71
ShashankShinde16 <119064985+ShashankShinde16@users.noreply.github.com>
2023-07-11 21:00:43 +0530

    python 1

---
+++ Python 1.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### python code
+
+# In[2]:
+
+
+Print("hello world")
+
+
+# In[3]:
+
+
+print(hello world)
+
+
+# In[5]:
+
+
+print(10)
+
+
+# In[7]:
+
+
+print(10.7)
+
+
+# #### code
+
+# ## after 
+
+# In[ ]:
+
+
+_name = "data"
+
+
+# In[4]:
+
+
+1name = "data"
+
+
+# In[ ]:
+
+
+print(_name)
+
+
+# ### data type
+
+# In[5]:
+
+
+x = 10.5
+
+
+# In[6]:
+
+
+type(x)
+
+
+# In[7]:
+
+
+x,type(x)
+
+
+# In[8]:
+
+
+x= 10
+y= 20
+y
+x
+
+
+# In[9]:
+
+
+x,y
+
+
+# In[10]:
+
+
+name = "shashank"
+type(name)
+age = 20
+type(age)
+
+
+# In[11]:
+
+
+type(name),type(age)
+
+
+# In[ ]:
+
+
+
+
